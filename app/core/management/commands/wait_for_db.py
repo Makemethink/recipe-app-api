d82fe42b2bdd0ff985719623fc8f001a8526a770
Swami <swaminathan300600@gmail.com>
2024-06-22 13:51:16 +0530

    added count for DB retry

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,14 +16,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Waiting for the DB ...')
 
+        count = 0
+        max_count = 10
+
         db_up: bool = False
         while db_up is False:
             try:
+                if max_count > 10:
+                    break
+
                 conn = mysql.connect(  # Replace with your database name
                     user=os.environ.get('DB_USER'),  # Replace with your MySQL username
                     password=os.environ.get('DB_PASS'),  # Replace with your MySQL password
                     host=os.environ.get('DB_HOST'),  # Replace with your MySQL host address
                     port=3306)
+
                 cursor = conn.cursor()
                 cursor.execute(f"CREATE DATABASE {os.environ.get('DB_NAME')}")
                 conn.close()
@@ -31,6 +38,7 @@ class Command(BaseCommand):
                 db_up = True
             except mysql.Error as e:
                 print(f'Error connecting to the database: {e}')
+                count = count+1
                 time.sleep(5)
 
         self.stdout.write(self.style.SUCCESS('Database available'))
